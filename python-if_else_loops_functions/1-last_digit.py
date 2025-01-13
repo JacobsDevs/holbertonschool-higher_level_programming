0986f6a1801c8d7d6ea55f6a1eeb3f257b298f67
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-14 10:26:36 +1100

    fix: 1* remove The from start of string

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 last = abs(number) % 10
 if number < 0:
     last = last * -1
-print(f"The last digit of {number} is {last} and is", end="")
+print(f"Last digit of {number} is {last} and is", end="")
 if last > 5:
     print(" greater than 5")
 elif last == 0:
