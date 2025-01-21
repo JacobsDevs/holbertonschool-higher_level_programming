004580808b4d0c863a8f412055d418caa13db8d1
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-21 11:43:18 +1100

    fix: 7-

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,8 +2,14 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     nums = [0, 0]
     for i, v in enumerate(tuple_a):
-        nums[i] += v
+        if i >= 2:
+            pass
+        else:
+            nums[i] += v
     for i, v in enumerate(tuple_b):
-        nums[i] += v 
+        if i >= 2:
+            pass
+        else:
+            nums[i] += v 
     tup = tuple(nums)
     return tup
