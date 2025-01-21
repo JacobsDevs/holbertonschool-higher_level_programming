7693c9caf3e397850cae155e4d4ee4af3109a6c4
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-22 10:54:57 +1100

    fix: 6-* whitespace

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 
 def print_sorted_dictionary(a_dictionary):
     for k in dict(sorted(a_dictionary.items())):
-        print("{}: {}".format(k,a_dictionary[k]))
+        print("{}: {}".format(k, a_dictionary[k]))
