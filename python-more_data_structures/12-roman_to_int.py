d8280d0dcfb6f32cba758edf09146e10b0242869
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-22 15:42:19 +1100

    fix: 12-* Refactor long line, rename translate to t

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return None
-    translation = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    t = {"I": 1, "V": 5, "X": 10, "L": 50,
+         "C": 100, "D": 500, "M": 1000}
     res = 0
     processed = False
     length = len(roman_string)
@@ -10,9 +11,9 @@ def roman_to_int(roman_string):
         if processed:
             processed = False
             continue
-        if i + 1 < length and translation[v] < translation[roman_string[i + 1]]:
-            res += translation[roman_string[i + 1]] - translation[v]
+        if i + 1 < length and t[v] < t[roman_string[i + 1]]:
+            res += t[roman_string[i + 1]] - t[v]
             processed = True
         else:
-            res += translation[v]
+            res += t[v]
     return res
