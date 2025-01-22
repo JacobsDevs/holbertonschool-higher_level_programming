5743233dfcbee4f47323983580097750aafb6d78
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-22 17:03:32 +1100

    fix: 4-list_division to include finally:

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,14 +6,14 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             res = my_list_1[i] / my_list_2[i]
         except TypeError:
-            results[i] = 0
+            res = 0
             print("wrong type")
         except ZeroDivisionError:
-            results[i] = 0
+            res = 0
             print("division by 0")
         except IndexError:
-            results[i] = 0
+            res = 0
             print("out of range")
-        else:
+        finally:
             results[i] = res
     return results
