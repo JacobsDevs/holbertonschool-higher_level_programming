a0d836f3ef98c06bdc56779004050bb5fa36ef40
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-24 17:36:22 +1100

    fix: 6-* Formatting

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -3,25 +3,27 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
 
     def test_basic(self):
-        self.assertEqual(max_integer([1,2,3]), 3)
+        self.assertEqual(max_integer([1, 2, 3]), 3)
 
     def test_basic_middle(self):
-        self.assertEqual(max_integer([1,3,2]), 3)
+        self.assertEqual(max_integer([1, 3, 2]), 3)
 
     def test_basic_reversed(self):
         self.assertEqual(max_integer([3, 2, 1]), 3)
 
     def test_two_max_values(self):
         self.assertEqual(max_integer([1, 2, 3, 3]), 3)
-    
+
     def test_one_value(self):
         self.assertEqual(max_integer([1]), 1)
 
     def test_no_values(self):
         self.assertEqual(max_integer(), None)
+
 
 if __name__ == "__main__":
     unittest.main()
