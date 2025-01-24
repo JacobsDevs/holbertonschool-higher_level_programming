626f21f3e44a87ffd803a722a5915e7f854deb0f
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-24 12:56:36 +1100

    fix: 0-* add float overflow & NaN tests. Fix pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,15 +6,20 @@ Args:
 Return:
     The result of a + b
 """
+
+
 def add_integer(a, b=98):
     """
     Return the result of a + b.
     """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+    if abs(a)==float('inf'):
+        raise OverflowError("float is overflowing")
     try:
         return int(a) + int(b)
-    except Exception:
-        raise Exception
+    except ValueError:
+        pass
