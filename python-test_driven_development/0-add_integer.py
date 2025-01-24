467483a2ffac0440324b2e501290769ddceb0a5e
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-24 12:37:13 +1100

    add: 0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This is the 0-add_integer module which contains the add_integer file:
+Args:
+    a: The first integer to be added.
+    b: The second integer to be added.
+Return:
+    The result of a + b
+"""
+def add_integer(a, b=98):
+    """
+    Return the result of a + b.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    try:
+        return int(a) + int(b)
+    except Exception:
+        raise Exception
