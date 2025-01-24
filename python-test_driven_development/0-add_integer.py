440270afcedf4dfa7a3bb00b30d1d7de64818a03
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-24 12:57:29 +1100

    fix: 0-* correct whitespace around operators

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -17,7 +17,7 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if abs(a)==float('inf'):
+    if abs(a) == float('inf'):
         raise OverflowError("float is overflowing")
     try:
         return int(a) + int(b)
