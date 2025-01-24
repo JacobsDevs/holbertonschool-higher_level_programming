3fa2b156441dd6f8d3dd140829696762f54976f7
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-24 15:07:06 +1100

    fix: 2-* pycodestyle and test missing args

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""This is the 2-matrix_divided module which contains the matrix_divided function:
+"""This is the 2-matrix_divided module which contains the
+   matrix_divided function:
 Args:
     matrix: The matrix to be divided.
     div: The integer to divide each element of the matrix by.
 Return:
     The matrix of results
 """
+
 
 def matrix_divided(matrix, div):
     """
@@ -17,14 +19,11 @@ def matrix_divided(matrix, div):
         if not isinstance(div, (int, float)):
             div_is_number = False
             raise TypeError
-        new_matrix = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
-        """for c, v in enumerate(matrix):
-            for d, j in enumerate(v):
-                new_matrix[c][d] = round(j / div, 2)"""
         if len(set(map(lambda x: len(x), matrix))) > 1:
             correct_length = False
             raise TypeError
-        return new_matrix
+        return list(
+            map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
 
     except TypeError:
         if not correct_length:
@@ -32,4 +31,5 @@ def matrix_divided(matrix, div):
         if not div_is_number:
             raise TypeError('div must be a number')
         else:
-            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            raise TypeError(
+                'matrix must be a matrix (list of lists) of integers/floats')
