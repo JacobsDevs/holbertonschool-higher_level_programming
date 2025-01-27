3f16002b2c78ae22971b68ca588e42b83f335757
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-28 10:47:00 +1100

    add: 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for the Square class.
+"""
+
+
+class Square():
+    """
+    A class defining Square
+    Attributes:
+        size (int): Private attribute contianing the length of a side
+        on the square.
+    """
+
+    def __init__(self, size):
+        self._size = size
