8c1e7c47735f7ca14bc013bd3cab6822dd1c7de0
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-29 09:22:32 +1100

    add: README & 0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Module containing the rectangle class.
+"""
+
+class Rectangle():
+    """
+    Rectangle Class definition
+    """
+    def __init__(self):
+        pass
