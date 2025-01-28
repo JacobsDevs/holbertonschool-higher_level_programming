92bcfc0b2be2aa02a56af304888d11eee338bcde
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-28 11:09:50 +1100

    add: 3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module for the Square class.
+"""
+
+
+class Square():
+    """
+    A class defining Square
+    Attributes:
+        size (int): Private attribute contianing the length of a side
+        on the square.
+    """
+
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size * self.__size
