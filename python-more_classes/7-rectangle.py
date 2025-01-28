a0a83c660959b58c322eb712d94feed48de13d08
JacobsDevs <jacobdevsmail@gmail.com>
2025-01-29 10:33:45 +1100

    fix: 7-rectangle.py print_symbol can be int

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -69,7 +69,7 @@ class Rectangle():
         string = ""
         for i in range(self.height):
             for j in range(self.width):
-                string += self.print_symbol
+                string += str(self.print_symbol)
             string += "\n"
         return string[0:-1]
 
