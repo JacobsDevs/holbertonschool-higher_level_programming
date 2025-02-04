83a9b62997acda7569d6d7a65af4dad028913a9e
JacobsDevs <jacobdevsmail@gmail.com>
2025-02-04 15:03:56 +1100

    fix: 2-is_same_class.py add Documentation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,6 +5,17 @@ This is the module containing the is_same_class function.
 
 
 def is_same_class(obj, a_class):
+    """
+    is_same_class:
+
+    Args:
+    @obj: The object to test
+    @a_class: The class to test obj against
+
+    Return:
+    True if exactly same class.
+    False if not.
+    """
     if isinstance(obj, a_class):
         return True
     else:
