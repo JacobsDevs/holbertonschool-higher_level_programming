8c873717d2a846cb6445a33eb763148ef9f5e09d
JacobsDevs <jacobdevsmail@gmail.com>
2025-02-04 15:29:29 +1100

    add: 4-inherits_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is the module that contains the inherits_from function
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    inherits_from:
+
+    Args:
+    @obj: The object to check.
+    @a_class: The class the check obj against.
+
+    Return:
+    True if a subclass.
+    False if not.
+    """
+
+    if issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
