0c5fe0ae9cec6862cf632fd553a7bb6ec98bf24e
JacobsDevs <jacobdevsmail@gmail.com>
2025-02-04 13:50:14 +1100

    fix: Added documentation to the 0-lookup.py module

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+This is the module containing the lookup function.
+"""
+
 
 def lookup(obj):
+    """
+    This is the lookup function
+
+    Args:
+    @obj: The object to lookup
+
+    Return:
+    A list of the objects methods and attributes.
+    """
     return dir(obj)
