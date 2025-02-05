8860c2cd90edf71977cc04559f9fc09bfaacc88e
JacobsDevs <jacobdevsmail@gmail.com>
2025-02-05 13:11:41 +1100

    fix: 8-rectangle.py Formatting

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,8 +4,8 @@ This is the module containing the Rectangle class.
 This class inherits from the BaseGeometry class located in 7-base_geometry.py
 """
 
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """ The Rectangle class
@@ -16,7 +16,6 @@ class Rectangle(BaseGeometry):
         @width: Length of the horizontal sides.
         @height: Length of the vertical sides.
     """
-
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
