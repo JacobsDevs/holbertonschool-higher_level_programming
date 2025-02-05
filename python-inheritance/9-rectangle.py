65499e45ef50c4681acafd57328ebf55aa401d25
JacobsDevs <jacobdevsmail@gmail.com>
2025-02-05 13:58:43 +1100

    add: 9-rectangle.py

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This is the module containing the Rectangle class.
+This class inherits from the BaseGeometry class located in 7-base_geometry.py
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ The Rectangle class
+    Inherits:
+        BaseGeometry
+
+    Args:
+        @width: Length of the horizontal sides.
+        @height: Length of the vertical sides.
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[{}] {}/{}".format(self.__class__.__name__,
+                                   self.__width,
+                                   self.__height)
