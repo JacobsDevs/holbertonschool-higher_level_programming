4c36e80df34c50f6381908c7d3a87c2f8a7ef565
JacobsDevs <jacobdevsmail@gmail.com>
2025-02-05 14:42:16 +1100

    add: task_00_abc.py

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Module containing the abstract class Animal.
+"""
+
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    Animal class inherits ABC
+    """
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    """
+    Dog class inherits from Animal
+
+    Abstract Methods:
+        sound(): a dog will Bark
+    """
+
+    def sound(self):
+        return "Woof"
+
+
+class Cat(Animal):
+    """
+    Cat class ingerits from Animal
+
+    Abstract Methods:
+        sound(): a cat will Meow
+    """
+
+    def sound(self):
+        return "Meow"
