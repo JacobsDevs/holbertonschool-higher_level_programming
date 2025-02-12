d8fd525171398c92437ffcbc2a85cb4a88489bba
JacobsDevs <jacobdevsmail@gmail.com>
2025-02-13 09:39:26 +1100

    fix: left erroneous code in 0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,12 +4,7 @@ This is the module including the read_file function
 """
 
 
-from os import read
-
-
 def read_file(filename=""):
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
-
-read_file('my_file_0.txt')
