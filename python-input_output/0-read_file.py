0dc51d3cea7297d2ac4f0caa405bf245df7bf027
JacobsDevs <jacobdevsmail@gmail.com>
2025-02-13 09:40:43 +1100

    fix: 0-read_file add documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,14 @@ This is the module including the read_file function
 
 
 def read_file(filename=""):
+    """Reads the file and prints it to the stdout
+
+    Args:
+    @filename: The filename of the file to read.
+
+    Returns:
+    Nothing
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
