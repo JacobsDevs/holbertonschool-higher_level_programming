6ae95002be95910fa8584379ceed65c6287b23bd
JacobsDevs <jacobdevsmail@gmail.com>
2025-02-13 14:37:35 +1100

    add: task_01_pickle.py and test

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module containing the CustomObject class.
+"""
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student) -> None:
+        self.name: str = name
+        self.age: int = age
+        self.is_student: bool = is_student
+
+    def display(self):
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except FileNotFoundError:
+            return None
---
+++ python-serialization/test_01_pickle.py
@@ -0,0 +1,19 @@
+import unittest
+from task_01_pickle import CustomObject
+
+class TestClass(unittest.TestCase):
+    def test_CustomObject_instantiation(self):
+        c = CustomObject("John", 7, True)
+        self.assertEqual(c.name, "John")
+        self.assertEqual(c.age, 7)
+        self.assertEqual(c.is_student, True)
+
+    def test_CustomObject_serialize_deserialize(self):
+        c = CustomObject("John", 7, True)
+        c.serialize("test_object.json")
+        b = CustomObject.deserialize("test_object.json")
+        self.assertEqual(b.__dict__, c.__dict__)
+
+    def test_deserialize_nonexistant_file(self):
+        b = CustomObject.deserialize("no_file.txt")
+        self.assertEqual(b, None)
