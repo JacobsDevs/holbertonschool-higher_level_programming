dacc0293489dac56703dcb19562ab21d41ab063f
JacobsDevs <JacobsDevsmail@gmail.com>
2025-03-06 11:26:25 +1100

    fix: 0-* to use SHOW GRANTS

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- Show the privileges of user_0d_1 and user_0d_2
-SELECT *
-FROM information_schema.user_privileges
-WHERE GRANTEE in ("'user_0d_1'@'localhost'", "'user_0d_2'@'localhost'");
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
