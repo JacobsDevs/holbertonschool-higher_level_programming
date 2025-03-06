d2ed67c0b02c9c72804dfbc2811014aa7e95163b
JacobsDevs <JacobsDevsmail@gmail.com>
2025-03-06 15:20:48 +1100

    Add: 13-count_shows_by_genre.sql

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Select the names of Genres and the count of shows in that genre.
+SELECT g.name, COUNT(s.title) AS number_of_shows
+FROM tv_genres AS g
+LEFT JOIN tv_show_genres AS j
+ON g.id = j.genre_id
+LEFT JOIN tv_shows AS s
+ON s.id = j.show_id
+GROUP BY g.name
+ORDER BY number_of_shows DESC;
