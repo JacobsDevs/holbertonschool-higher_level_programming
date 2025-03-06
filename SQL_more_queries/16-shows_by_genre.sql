ecf5c11961898670c4a1e747c39d323f1590662b
JacobsDevs <JacobsDevsmail@gmail.com>
2025-03-06 15:57:59 +1100

    add: 16-shows_by_genre.sql

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,14 @@
+SELECT s.title, g.name
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS j
+ON s.id = j.show_id
+LEFT JOIN tv_genres AS g
+ON j.genre_id = g.id
+UNION
+SELECT s.title, g.name
+FROM tv_shows AS s
+RIGHT JOIN tv_show_genres AS j
+ON s.id = j.show_id
+RIGHT JOIN tv_genres AS g
+ON g.id = j.genre_id
+ORDER BY title, name;
