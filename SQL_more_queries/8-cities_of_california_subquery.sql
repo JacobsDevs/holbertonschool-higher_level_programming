a06206a1663bfdb8219f4eb5f84109d306e18e6a
JacobsDevs <JacobsDevsmail@gmail.com>
2025-03-06 12:06:03 +1100

    fix: 8-* to not use JOIN

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,9 @@
 -- Lists the cities of California that can be found in the DB hbtn_0d_usa
 SELECT cities.id, cities.name 
-FROM cities
-INNER JOIN states ON cities.state_id = states.id
+FROM (
+	SELECT * FROM cities
+	UNION
+	SELECT * FROM states
+)
 WHERE states.name = "California"
 ORDER BY cities.id;
