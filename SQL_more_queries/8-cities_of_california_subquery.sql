50f1b1e3e7db0beae926efae285df0357bb8c792
JacobsDevs <JacobsDevsmail@gmail.com>
2025-03-06 14:42:10 +1100

    fix: 8-* Simplfy query

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- Select cities that are in California
 SELECT c.id, c.name 
-FROM cities as c 
+FROM cities AS c 
 WHERE c.state_id = (
 	SELECT s.id 
-	FROM states as s 
+	FROM states AS s 
 	WHERE s.name = "California")
 ORDER BY c.id;
