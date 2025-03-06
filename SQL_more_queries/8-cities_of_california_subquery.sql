d12531298d8ee46e33fe8737dbe31098daf931b4
JacobsDevs <JacobsDevsmail@gmail.com>
2025-03-06 12:12:20 +1100

    fix: 8-* query now aliases correct tables

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,8 @@
 -- Lists the cities of California that can be found in the DB hbtn_0d_usa
-SELECT cities.id, cities.name 
-FROM (
-	SELECT * FROM cities AS cities
+SELECT cities.a.id, cities.a.name FROM (
+	SELECT id, name FROM cities as cities
 	UNION
-	SELECT * FROM states AS states
-)
-WHERE states.name = "California"
-ORDER BY cities.id;
+	SELECT * FROM states as states
+) as a
+WHERE states.a.name = "California"
+ORDER BY cities.a.id;
