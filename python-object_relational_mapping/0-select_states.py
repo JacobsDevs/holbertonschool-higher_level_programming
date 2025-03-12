f3e3e27b0edb1a448a8f249eab1bc15a007918e3
JacobsDevs <JacobsDevsmail@gmail.com>
2025-03-12 13:18:21 +1100

    fix: 0-* Correct query

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,14 +4,16 @@ import sys
 import MySQLdb
 
 
-
 def print_all_rows():
     """This is the function that does it"""
-    db = MySQLdb.connect(host='localhost:3306', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost',
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     cur = db.cursor()
-    results = cur.execute("SELECT * FROM states ORDER BY id")
-    rows = results.fetchall()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
     for r in rows:
         print(r)
 
