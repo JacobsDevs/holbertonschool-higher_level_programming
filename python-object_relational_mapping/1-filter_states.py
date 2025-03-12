f16f9a3bb28dfec62aa2874e90c5f1567f17434d
JacobsDevs <JacobsDevsmail@gmail.com>
2025-03-12 13:32:44 +1100

    fix: Case Sensitive

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ def print_all_rows():
                          db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'n%' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
     rows = cur.fetchall()
     for r in rows:
         print(r)
