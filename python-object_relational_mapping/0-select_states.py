6af1bbbb34f640054bc986107f5cfcf92a43bd49
JacobsDevs <JacobsDevsmail@gmail.com>
2025-03-12 12:45:45 +1100

    add: 0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost:3306', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    results = cur.execute("SELECT * FROM states ORDER BY id")
+    rows = results.fetchall()
+    for r in rows:
+        print(r)
