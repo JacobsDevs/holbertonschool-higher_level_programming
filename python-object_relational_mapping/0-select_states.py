3d550f50d1c161f9a17b98e5f4a41d2f875de426
JacobsDevs <JacobsDevsmail@gmail.com>
2025-03-12 12:46:44 +1100

    fix: Add Documentation to 0-*

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,6 +3,9 @@ import sys
 import MySQLdb
 
 
+"""
+Prints a list of the states in the database
+"""
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost:3306', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
