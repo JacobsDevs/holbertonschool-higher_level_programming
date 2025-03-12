191fa0be6f678904a62fb121b1973c396cfeab63
JacobsDevs <jacobdevsmail@gmail.com>
2025-03-13 10:56:24 +1100

    add: 7-model_state_fetch.py

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,16 @@
+#!/home/jacob/.local/share/mise/installs/python/3.13.1/bin/python
+"""Lists the states in the database using sqlalchemy"""
+import sys
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    session = Session(engine)
+
+    stmt = select(State)
+
+    for state in session.scalars(stmt):
+        print("{}: {}".format(state.id, state.name))
