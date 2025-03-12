453721c813bddbf01d410bf1a74fbb4df82aebe0
JacobsDevs <jacobdevsmail@gmail.com>
2025-03-13 10:37:52 +1100

    fix: model_state - Add Doc

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,6 +7,14 @@ from sqlalchemy import Nullable, create_engine, Column, Integer, String, Null
 Base = declarative_base()
 
 class State(Base):
+    """State Class
+
+    Inherits: DeclarativeBase from sqlalchemy.
+
+    Args:
+    @id: The id in the databasse, NOT NULL, AUTOINCREMENTING UNIQUE PK.
+    @name: The name of the state.
+    """
     __tablename__ = 'states'
 
     id = Column(Integer,
