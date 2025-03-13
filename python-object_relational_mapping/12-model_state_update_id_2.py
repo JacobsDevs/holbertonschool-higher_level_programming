f57cbb1c22472487f2c2beca45bf66e6a83b824b
JacobsDevs <jacobdevsmail@gmail.com>
2025-03-13 12:18:04 +1100

    add: 12-model_state_update_id_2.py

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Lists the first state in the database using sqlalchemy"""
+import sys
+from sqlalchemy import create_engine, update
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    session = Session(engine)
+
+    query = update(State).where(State.id == 2).values(name="New Mexico")
+
+    with engine.connect() as conn:
+        result = conn.execute(query)
+        conn.commit()
