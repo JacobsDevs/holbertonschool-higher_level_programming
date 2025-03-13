b6b357d64b7c034ee980dfd5b9f855260c2ab1c4
JacobsDevs <jacobdevsmail@gmail.com>
2025-03-13 12:13:00 +1100

    add: 11-model_state_insert.py

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Lists the first state in the database using sqlalchemy"""
+import sys
+from sqlalchemy import create_engine, insert
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    session = Session(engine)
+
+    query = insert(State).values(name="Louisiana")
+
+    with engine.connect() as conn:
+        result = conn.execute(query)
+        conn.commit()
+        print(result.inserted_primary_key)
